1b07af9180e53015ad5a45a26c724f89d4196c64
TasnimKulle <tasniim.tak@gmail.com>
2025-03-04 05:15:20 +0300

    responsive menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,9 @@
 const toggleButton = document.querySelector('.toggle-button')
 const header=document.querySelector('header')
-const logInOut=document.querySelector(".log-in a")
 const navLinks = document.querySelector('.navlink a')
 toggleButton.addEventListener('click',function(){
-    navLinks.classList.toggle('active');
-    logInOut.classList.toggle('active')
+    header.classList.toggle('active');
+    // navLinks.classList.toggle('active')
     console.log('clicked', navLinks)
 
 })
