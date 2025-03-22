2c280f0784dbe1be3568e1b815015518c155ff32
TasnimKulle <tasniim.tak@gmail.com>
2025-03-22 12:23:30 +0300

    declaring variable

---
+++ scripts/auth.js
@@ -0,0 +1,8 @@
+const authSwitch=document.querySelector('#authSwitch');
+const switchForm =document.querySelector('siwtchForm');
+const authForm=document.querySelector('#authForm');
+const email=document.querySelector('#email');
+const username=document.querySelector('#username');
+const password=document.querySelector('#password');
+const confirmPassword=document.querySelector('#confimPassword');
+const authButton=document.querySelector('#authButton')
