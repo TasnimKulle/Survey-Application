eb0b2c62d9f04a8347c5d1908dd05e0f39bf9e8e
TasnimKulle <tasniim.tak@gmail.com>
2025-04-22 12:31:57 +0300

    adding online user and local API Data questioners as json file

---
+++ scripts/useronline.js
@@ -0,0 +1,19 @@
+function toggleDropdown() {
+    const dropdown = document.getElementById("profileDropdown");
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+  }
+  
+  const user = JSON.parse(localStorage.getItem("OnlineUsers"));
+  
+  if (!user) {
+    window.location.href = '/index.html';
+  } else {
+    document.getElementById("username").textContent = `${user.username || 'N/A'}`;
+    document.getElementById("email").textContent = `${user.email}`;
+  }
+  
+  function logout() {
+    localStorage.removeItem("OnlineUsers");
+    window.location.href = '/index.html';
+  }
+  
